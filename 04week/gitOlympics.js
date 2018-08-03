5295a83190378f896382090d63968f0ec0dfa5e2
sarahcheatham <sarah.cheatham14@gmail.com>
2018-08-02 19:23:14 -0500

    test

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const printListOfOlympians = (namesArray) => {
+    namesArray.forEach(function(element){
+        console.log(element)
+    });
+}
+
+printListOfOlympians(['Justin', 'Quincy', 'Sarah', 'Wade', 'Renee'])
