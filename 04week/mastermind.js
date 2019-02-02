142bfe2972e8262aff93bc3699fc5ba505a6fc25
sarahcheatham <sarah.cheatham14@gmail.com>
2019-02-01 18:53:42 -0600

    working on generate hint

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const readline = require('readline');
+const colors = require('colors');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -11,6 +12,7 @@ let board = [];
 let solution = 'abcd';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 let correctLetterLocations = 0;
+let correctLetters = 0;
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
@@ -32,16 +34,24 @@ function getRandomInt(min, max) {
 const generateHint = (solution, guess)=> {
   const solutionArray = solution.split('');
   const guessArray = guess.split('');
+  const targetIndex = [];
   for(let i = 0; i < solutionArray.length; i++){
     for(let j = 0; j < guessArray.length; j++){
-      if(solutionArray[i] === guessArray[j]){
-        correctLetterLocations++
-        console.log(correctLetterLocations)
+      if(i === j && solutionArray[i] === guessArray[j]){
+        solutionArray[i] = null;
+        correctLetterLocations++;
+        const red = colors.red(solutionArray[i]);
+        console.log(red)
+      } else if(solutionArray[i] === guessArray[j]){
+        const letterIndex = guessArray.indexOf(solutionArray[i]);
+        targetIndex.push(letterIndex)
+        correctLetters++;
+        const hypenLetters = `-${solutionArray[i]}-`;
+        // console.log(hypenLetters)
+        return hypenLetters
       }
-      console.log(`solution[i]: ${solutionArray[i]}, guess[j]: ${guessArray[j]}`)
     }
   }
-  console.log(`guess: ${guessArray}`)
 }
 
 const mastermind = (guess)=> {
@@ -49,7 +59,9 @@ const mastermind = (guess)=> {
   if(guess === solution){
     console.log('You guessed it!');
   } else {
-    generateHint(solution, guess)
+    const hint = generateHint(solution, guess);
+    board.push(`${guess} ${hint}`)
+    console.log(board)
   }
   console.log(typeof solution)
 }
